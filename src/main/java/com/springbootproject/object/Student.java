3dd4ad9f32db7ccccbb569e192af113d8f39b03c
Simon <simonandraspeter@gmail.com>
2024-01-29 09:02:47 +0100

    Simon - Student CRUD + Entity merging with Farun's version

--- src/main/java/com/springbootproject/object/Student.java
+++ src/main/java/com/springbootproject/object/Student.java
@@ -2,6 +2,7 @@ package com.springbootproject.object;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,15 +17,20 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column
     @NotBlank(message = "name must have a value")
     private String name;
+
     @Column
+    @Min(value = 1, message= "must have a value and be above 1")
     private int age;
+
     @NotBlank(message = "course must have a value")
     @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "studentList")
     private Course course;
+
     @Column
     @NotBlank(message = "email must have a value")
     @Email
