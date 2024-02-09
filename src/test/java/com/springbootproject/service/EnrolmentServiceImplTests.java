a6ab77985ac6288f2e286487feacb94a0adde37c
o-lat-ukr <olatsko@gmail.com>
2024-02-09 19:46:53 +0200

    Added (revised) tests to EnrollmentServiceImpl

--- src/test/java/com/springbootproject/service/EnrolmentServiceImplTests.java
+++ src/test/java/com/springbootproject/service/EnrolmentServiceImplTests.java
@@ -6,10 +6,7 @@ import com.springbootproject.object.Course;
 import com.springbootproject.object.Student;
 import com.springbootproject.repository.CourseRepository;
 import com.springbootproject.repository.StudentRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -22,6 +19,7 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class EnrolmentServiceImplTests {
@@ -70,35 +68,106 @@ public class EnrolmentServiceImplTests {
         unenrolledStudent.setEmail("vadams@gmail.com");
     }
 
+    @BeforeEach
+    public void setTestingObjects() {
+        enrolledStudent.setCourse(testCourse1);
+        unenrolledStudent.setCourse(null);
+    }
+
+    // ENROLL METHOD
 
     @Test
-    @DisplayName("#1: Non-existing student should throw exception")
-    public void nonExistingStudentShouldThrowException() {
-        Mockito.lenient().when(studentRepository.findById(anyInt())).thenReturn(Optional.empty()); // IDE says that this line is not used. I do not understand the reason behind it yet
-        Assertions.assertThrows(ElementNotFoundException.class, () -> enrolmentService.enroll(anyInt(), 1));
+    @DisplayName("#1.1: Non-existing student should throw exception")
+    public void nonExistingStudentShouldThrowException1() {
+        when(studentRepository.findById(anyInt())).thenReturn(Optional.empty());
+        Assertions.assertThrows(ElementNotFoundException.class, () -> enrolmentService.enroll(anyInt(), testCourse1.getId()));
     }
 
     @Test
-    @DisplayName("#2: Non-existing course should throw exception")
-    public void nonExistingCourseShouldThrowException() {
-        Mockito.lenient().when(courseRepository.findById(anyInt())).thenReturn(Optional.empty()); // IDE says that this line is not used. I do not understand the reason behind it yet
-        Assertions.assertThrows(ElementNotFoundException.class, () -> enrolmentService.enroll(unenrolledStudent.getId(), -1));
+    @DisplayName("#1.2: Non-existing course should throw exception")
+    public void nonExistingCourseShouldThrowException1() {
+        when(studentRepository.findById(anyInt())).thenReturn(Optional.of(unenrolledStudent));
+        when(courseRepository.findById(anyInt())).thenReturn(Optional.empty());
+       Assertions.assertThrows(ElementNotFoundException.class, () -> enrolmentService.enroll(unenrolledStudent.getId(), anyInt()));
     }
 
     @Test
-    @DisplayName("#3: Enrolled student can't be re-enrolled")
+    @DisplayName("#1.3: Enrolled student can't be re-enrolled")
     public void enrolledStudentCantBeEnrolledAgain() {
-        Mockito.when(studentRepository.findById(anyInt())).thenReturn(Optional.of(enrolledStudent));
-        Assertions.assertThrows(EnrollmentStatusException.class, () -> enrolmentService.enroll(enrolledStudent.getId(), testCourse2.getId()));
+        int studentId = enrolledStudent.getId();
+        int courseId = testCourse1.getId();
+        when(studentRepository.findById(studentId)).thenReturn(Optional.of(enrolledStudent));
+        Assertions.assertThrows(EnrollmentStatusException.class, () -> enrolmentService.enroll(studentId, courseId));
     }
 
     @Test
-    @DisplayName("#4: Unenrolled existing student should enrol to existing course")
+    @DisplayName("#1.4: Correct enrolling student does not throw exception")
+    public void correctStudentShouldNotThrowException1() {
+        int studentId = unenrolledStudent.getId();
+        int courseId = testCourse2.getId();
+        when(studentRepository.findById(studentId)).thenReturn(Optional.of(unenrolledStudent));
+        when(courseRepository.findById(courseId)).thenReturn(Optional.of(testCourse2));
+        Assertions.assertDoesNotThrow(() -> enrolmentService.enroll(studentId, courseId));
+    }
+
+    @Test
+    @DisplayName("#1.5: Unenrolled existing student should enrol to existing course")
     public void unenrolledStudentDShouldEnrollSuccessfully() {
-        Mockito.when(studentRepository.findById(unenrolledStudent.getId())).thenReturn(Optional.of(unenrolledStudent));
-        Mockito.when(courseRepository.findById(testCourse2.getId())).thenReturn(Optional.of(testCourse2));
-        Student result = enrolmentService.enroll(unenrolledStudent.getId(), testCourse2.getId());
-        Assertions.assertNotNull(result.getCourse(), "Student has not been enrolled");
-        Assertions.assertEquals(testCourse2, result.getCourse(), "Student was not enrolled to a correct course");
+        int studentId = unenrolledStudent.getId();
+        int courseId = testCourse2.getId();
+        when(studentRepository.findById(studentId)).thenReturn(Optional.of(unenrolledStudent));
+        when(courseRepository.findById(courseId)).thenReturn(Optional.of(testCourse2));
+        Student result = enrolmentService.enroll(studentId, courseId);
+        Assertions.assertNotNull(result.getCourse(), "Student should have been enrolled, but he/she did not");
+        Assertions.assertEquals(testCourse2, result.getCourse(), String.format("Student should have been enrolled to %s, but he/she enrolled to %s", testCourse2.getName(), result.getCourse().getName()));
+    }
+
+
+    // UNENROLL METHOD
+
+    @Test
+    @DisplayName("#2.1: Non-existing unenrolling student should throw exception")
+    public void nonExistingStudentShouldThrowException2() {
+       when(studentRepository.findById(anyInt())).thenReturn(Optional.empty());
+       Assertions.assertThrows(ElementNotFoundException.class, () -> enrolmentService.unenroll(anyInt(), testCourse1.getId()));
+    }
+
+    @Test
+    @DisplayName("#2.2: Non-existing course should throw exception")
+    public void nonExistingCourseShouldThrowException2() {
+        when(studentRepository.findById(anyInt())).thenReturn(Optional.of(unenrolledStudent));
+        when(courseRepository.findById(anyInt())).thenReturn(Optional.empty());
+        Assertions.assertThrows(ElementNotFoundException.class, () -> enrolmentService.unenroll(unenrolledStudent.getId(), -1));
+    }
+
+    @Test
+    @DisplayName("#2.3: Unenrolled student can't be unenrolled")
+    public void unenrolledStudentCantBeUnenrolledAgain() {
+        int studentId = unenrolledStudent.getId();
+        when(studentRepository.findById(studentId)).thenReturn(Optional.of(unenrolledStudent));
+        when(courseRepository.findById(anyInt())).thenReturn(Optional.of(testCourse1)); // to ensure that 404 exception is not thrown
+        Assertions.assertThrows(EnrollmentStatusException.class, () -> enrolmentService.unenroll(studentId, anyInt()));
+    }
+
+    @Test
+    @DisplayName("#2.4: Wrong course to unenroll")
+    public void wrongCourseToUnenroll() {
+        int studentId = enrolledStudent.getId(); // enrolled in testCourse1
+        int courseId = testCourse2.getId(); // testCourse2
+        when(studentRepository.findById(studentId)).thenReturn(Optional.of(enrolledStudent));
+        when(courseRepository.findById(courseId)).thenReturn(Optional.of(testCourse2)); // to ensure that 404 exception is not thrown
+        Assertions.assertThrows(EnrollmentStatusException.class, () -> enrolmentService.unenroll(studentId, courseId));
+    }
+
+    @Test
+    @DisplayName("#2.5: Correct unenrolling student does not throw exception")
+    public void correctStudentShouldNotThrowException2() {
+        int studentId = enrolledStudent.getId(); // enrolled in testCourse1
+        int courseId = enrolledStudent.getCourse().getId(); // testCourse1
+
+        when(studentRepository.findById(studentId)).thenReturn(Optional.of(enrolledStudent));
+        when(courseRepository.findById(courseId)).thenReturn(Optional.of(enrolledStudent.getCourse()));
+        when(studentRepository.save(any(Student.class))).thenAnswer(i -> i.getArgument(0));
+        Assertions.assertDoesNotThrow(() -> enrolmentService.unenroll(studentId, courseId));
     }
 }
